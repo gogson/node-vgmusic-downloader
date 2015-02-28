8e741c7b0086029f5f535bbf4fc013e598bbec4e
Hugo <hugo@placeloop.com>
2015-02-28 15:22:04 +0100

    [clean] added lib folder for helper and crawling functions

--- vgmusic.js
+++ vgmusic.js
@@ -4,56 +4,29 @@
 
 var fs = require('fs');
 var path = require('path');
-var crawler = require("simplecrawler");
-var helpers = require("./helpers");
 var config = require("./config");
+var helpers = require("./lib/helpers");
+var crawler = require("./lib/crawler");
 var colors = require('colors');
-
-// Helpers
- 
-fs.mkdirParent = function(dirPath, mode, callback) {
-  fs.mkdir(dirPath, mode, function(error) {
-    if (error && error.errno === 34) {
-      fs.mkdirParent(path.dirname(dirPath), mode, callback);
-      fs.mkdirParent(dirPath, mode, callback);
-    }
-    callback && callback(error);
-  });
-};
-
-//
-// Page Crawler
-//
-
-var pageCrawler = function(page, i) {
-  var current = page;
-  console.log(('* Starting Crawler #'+i+' for page\t' + current.replace(config.domain, '')+'...').blue);
-  crawler.crawl(current, function(queueItem, responseBuffer, response) {
-    var destination_dir = config.downloadFolder+"/"+current.split('/music/')[1];
-    var file_name = queueItem.url.split('/')[queueItem.url.split('/').length - 1];
-    if ("audio/midi" === response.headers['content-type'])
-    {
-      console.log("Crawler #"+i+("\tDownloaded " + file_name + " ("+(responseBuffer.length / 1000)+" kb) in %dms").blue, queueItem.stateData.downloadTime);
-      fs.mkdirParent(destination_dir);
-      fs.writeFile((destination_dir+"/"+file_name), responseBuffer, function(err) { 
-        console.log((err) ? ('Crawler #'+i+('\tError saving the MIDI file '+file_name+'\n'+err+'').red): ('Crawler #'+i+('\tSaved file '+file_name+' !').blue));
-      });
-    }
-  });
-}
 
 //
 //  Main
 //
 
-console.log('========================================================================');
-console.log('============           VGMusic MIDI Files Downloader        ============');
-console.log('========================================================================\n');
-console.log(('Starting crawling '+config.pages.length+' pages.\n').yellow);
+var main = function() {
+  
+  console.log('========================================================================');
+  console.log('============           VGMusic MIDI Files Downloader        ============');
+  console.log('========================================================================\n');
+  console.log(('Starting crawling '+config.pages.length+' pages.\n').yellow);
 
-fs.mkdirParent(config.downloadFolder);
+  fs.mkdirParent(config.downloadFolder);
 
-for (var i = 0; i < config.pages.length; i++)
-  pageCrawler((config.domain + '/' + config.pages[i]), i);
+  for (var i = 0; i < config.pages.length; i++)
+    crawler.pageCrawler((config.domain + '/' + config.pages[i]), i);
 
-console.log('\nPlease wait until all links are crawled...\n'.yellow);
+  console.log('\nPlease wait until all links are crawled...\n'.yellow);
+
+};
+
+main();
